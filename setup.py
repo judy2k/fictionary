7a20d819ab5e8391baba6c7722ed4eb8411432cf
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-09-10 17:18:50 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="fictionary",
+    version="0.0.0",
+    description="Generate made-up words following the patterns used by real English words.",
+    url="https://github.com/judy2k/fictionary",
+    author="Mark Smith",
+    author_email="mark.smith@practicalpoetry.co.uk",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords="words dictionary fictionary",
+    entry_points={
+        'console_scripts': [
+            'fictionary=fictionary:main',
+        ]
+    },
+)
