059cbeb3fbf5cd17f10f1a65aadde95a7a8e2719
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-09-12 13:59:58 +0100

    Added test for impossible generation exception

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -7,3 +7,8 @@ def test_predictable():
     markov.feed("abcde")
     assert ''.join(markov.random_sequence(5, 5)) == "abcde"
 
+
+def test_impossible():
+    markov = fictionary.Markov()
+    markov.feed("abcdefghi")
+    assert ''.join(markov.random_sequence(5, 5)) == 'abcde'
