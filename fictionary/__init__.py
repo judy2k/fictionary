81dce0f85246b318bb5e0bce3e42cc0dd4b08281
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-09-17 15:07:01 +0100

    PEP-8 and Flake8

--- fictionary/__init__.py
+++ fictionary/__init__.py
@@ -18,7 +18,6 @@ import sys
 
 import click
 
-
 APP_NAME = "fictionary"
 
 # Where to save the generated data file:
@@ -27,15 +26,16 @@ DATA_FILE_ROOT = click.get_app_dir(APP_NAME)
 # Where to load the source ispell wordlists:
 SRC_DATA_FILE_ROOT = dirname(__file__)
 
-
 LOG = logging.getLogger(__name__)
 
 ISPELL_FILESETS = {
     'all': glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/*.*')),
-    'british': glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/english.*')) +
-    glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/british.*')),
-    'american': glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/english.*')) +
-    glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/american.*')),
+    'british':
+        glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/english.*')) +
+        glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/british.*')),
+    'american':
+        glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/english.*')) +
+        glob(join(SRC_DATA_FILE_ROOT, 'ispell_wordlist/american.*')),
 }
 
 
@@ -45,6 +45,7 @@ class Markov(object):
     Values are implemented as Counter objects, providing a weighting for each
     successive token.
     """
+
     def __init__(self):
         self.data = {}
 
@@ -75,16 +76,16 @@ class Markov(object):
 
         for _ in range(1000):
             result = list(self.random_sequence_generator())
-            if (
-                len(result) >= min_length
-                and (max_length is None or len(result) <= max_length)
-                and filter(result)
-            ):
-                LOG.debug('Result: %s (%d <= %d <= %r)', result, min_length, len(result), max_length)
+            if len(result) >= min_length \
+                    and (max_length is None or len(result) <= max_length) \
+                    and filter(result):
+                LOG.debug(
+                    'Result: %s (%d <= %d <= %r)',
+                    result, min_length, len(result), max_length
+                )
                 return result
-        else:
-            raise Exception("Couldn't find a valid word in 1000 iterations - "
-                            "it looks like something is wrong!")
+        raise Exception("Couldn't find a valid word in 1000 iterations - "
+                        "it looks like something is wrong!")
 
     def random_sequence_generator(self):
         """ A generator to provide a sequence from the Markov model. """
@@ -102,6 +103,7 @@ class RandomCounter(Counter):
     """ A Counter with extra methods for choosing random keys from the
     collection, accounting for weightings provided by the counter values.
     """
+
     def pick(self, i):
         """ Pick a key from Counter, using the key's count as a weighting.
 
@@ -137,8 +139,9 @@ class DataFile(object):
     A data file containing pickled markov chains and a dict of known words.
     """
 
-    def __init__(self, path, filesets=ISPELL_FILESETS, refresh=False):
-        self.open_data_file(path, filesets, refresh)
+    def __init__(self, path, filesets=None, refresh=False):
+        self._shelf = None
+        self.open_data_file(path, filesets or ISPELL_FILESETS, refresh)
         self.path = path
 
     def __getitem__(self, key):
@@ -162,10 +165,10 @@ class DataFile(object):
         Read through one or more wordlist files and generate a Markov object
         representing the words.
 
-        ``files`` should be a sequence of file paths. Each file will be opened, and
-        each line should contain a single word.  Words beginning with a capital
-        letter or containing an apostrophe will be rejected. Each word is fed into
-        a Markov object as a sequence of characters.
+        ``files`` should be a sequence of file paths. Each file will be opened,
+        and each line should contain a single word.  Words beginning with a
+        capital letter or containing an apostrophe will be rejected. Each word
+        is fed into a Markov object as a sequence of characters.
         """
         result = Markov()
         for path in files:
@@ -185,7 +188,8 @@ class DataFile(object):
         if force_refresh or not exists(data_file_path):
             self.generate_data_file(data_file_path, filesets)
         else:
-            self._shelf = shelve.open(data_file_path, protocol=2, flag='w', writeback=True)
+            self._shelf = shelve.open(
+                data_file_path, protocol=2, flag='w', writeback=True)
 
     def generate_data_file(self, data_file_path, filesets):
         """
@@ -195,15 +199,22 @@ class DataFile(object):
         containing_dir = dirname(data_file_path)
         if not exists(containing_dir):
             makedirs(containing_dir)
-        self._shelf = shelve.open(data_file_path, protocol=2, flag='n', writeback=True)
+        self._shelf = shelve.open(
+            data_file_path, protocol=2, flag='n', writeback=True)
         self._shelf['wordlist'] = set()
         for dictionary in ['all', 'british', 'american']:
-            print("Generating '%s' dictionary... " % dictionary, file=sys.stderr)
+            print("Generating '%s' dictionary... " % dictionary,
+                  file=sys.stderr)
             sys.stderr.flush()
-            self._shelf[dictionary] = self.generate_word_list(filesets[dictionary])
+            self._shelf[dictionary] = self.generate_word_list(
+                filesets[dictionary]
+            )
             print('Done.', file=sys.stderr)
 
     def is_real_word(self, word):
+        """
+        Checks the provided word to see if it's contained in the data file.
+        """
         return word in self._shelf['wordlist']
 
 
@@ -213,38 +224,49 @@ def main(argv=sys.argv[1:]):
     """
     try:
         parser = argparse.ArgumentParser(description=__doc__.strip())
-        parser.add_argument('-v', '--verbose', action='store_true',
-                            help="Be verbose.")
-        parser.add_argument('-c', '--count', type=int, default=1,
-                            help="The number of words to generate.")
-        parser.add_argument('-m', '--min-length', type=int, default=4,
-                            metavar="LENGTH",
-                            help="Only generate words of LENGTH chars or longer.")
-        parser.add_argument('-x', '--max-length', type=int, default=None,
-                            metavar="LENGTH",
-                            help="Only generate words of LENGTH chars or shorter.")
-        parser.add_argument('--refresh', action='store_true',
-                            help="Re-create the data file from the word-lists.")
-        parser.add_argument('-d', '--dictionary', default='british',
-                            help="The dictionary rules to follow: american,"
-                            "british, or all")
+        parser.add_argument(
+            '-v', '--verbose', action='store_true', help="Be verbose.")
+        parser.add_argument(
+            '-c', '--count', type=int, default=1,
+            help="The number of words to generate.")
+        parser.add_argument(
+            '-m', '--min-length', type=int, default=4,
+            metavar="LENGTH",
+            help="Only generate words of LENGTH chars or longer.")
+        parser.add_argument(
+            '-x', '--max-length', type=int, default=None, metavar="LENGTH",
+            help="Only generate words of LENGTH chars or shorter.")
+        parser.add_argument(
+            '--refresh', action='store_true',
+            help="Re-create the data file from the word-lists.")
+        parser.add_argument(
+            '-d', '--dictionary', default='british',
+            help="The dictionary rules to follow: american, british, or all")
 
         args = parser.parse_args(argv)
 
         if args.max_length is not None:
             if args.min_length > args.max_length:
-                print("Words cannot have a max-length shorter than their min-length!", file=sys.stderr)
+                print("Words cannot have a max-length shorter than their"
+                      " min-length!", file=sys.stderr)
                 return -1
 
         LOG.setLevel(logging.DEBUG if args.verbose else logging.WARNING)
 
-        with DataFile(join(DATA_FILE_ROOT, 'dictionary.dat'), refresh=args.refresh) as shelf:
+        path = join(DATA_FILE_ROOT, 'dictionary.dat')
+        with DataFile(path, refresh=args.refresh) as shelf:
             model = shelf[args.dictionary]
             for _ in range(args.count):
-                print(''.join(model.random_sequence(args.min_length, args.max_length, lambda w: not shelf.is_real_word(''.join(w)))))
-    except KeyboardInterrupt: pass
+                real_word_filter = lambda w: not shelf.is_real_word(''.join(w))
+                print(''.join(
+                    model.random_sequence(
+                        args.min_length, args.max_length, real_word_filter)
+                ))
+    except KeyboardInterrupt:
+        pass
 
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
